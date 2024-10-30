c62c1b9369a70a7cfb7b3aa5bf00bbe6d36873b1
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2024-10-30 10:29:58 -0300

    30/10/2024

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+'pais';
+' Qual o maior pais do mundo?'
+' russia'
+)
+criaCartao(
+    'Bebida'
+ ' Qual a bebida mais vendida do mundo?'  
+ 'Cafe' 
+)
+criaCartao(
+'Animal'
+'Qual o animal mais bravo?'
+'Tubarao branco'
+)
+criaCartao(
+    'Carro'
+    'Qual e o carro mais rapido do brasil?'
+    'Lamborghini'
+)
